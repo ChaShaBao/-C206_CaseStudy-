0a780f25c58c87395b846831ff871d1c808eef36
joshl <joshl@JeshLim>
2023-07-27 02:32:19 +0800

    Class to represent Service (updated)

---
+++ C206_CaseStudy/src/Service.java
@@ -0,0 +1,30 @@
+class Service {
+    String serviceName;
+    String description;
+    double price;
+
+    public Service(String serviceName, String description, double price) {
+        this.serviceName = serviceName;
+        this.description = description;
+        this.price = price;
+    }
+    public String getService() {
+        return serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+    	return price;
+    }
+    public void setService(String service) {
+        this.serviceName = service;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
