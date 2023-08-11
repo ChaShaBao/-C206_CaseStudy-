bbbab2af1eda770e2cae67655dc458dc35c511e1
joshl <joshl@JeshLim>
2023-08-11 23:54:53 +0800

    Request class (updated)

--- C206_CaseStudy/src/Request.java
+++ C206_CaseStudy/src/Request.java
@@ -1,33 +1,61 @@
+import java.util.Date;
+
 public class Request {
      int requestId;
+     User user;
      Service service;
-     String projectDescription;
-     String requirements;
+     String projectDescription;  
+     Date requestDate;
+     Date startDate;
+     double budget;
 
-    public Request(int requestId, Service service, String projectDescription, String requirements) {
+    public Request(int requestId, User user ,Service service, String projectDescription, Date requestDate, Date startDate,double budget) {
         this.requestId = requestId;
+        this.user=user;
         this.service = service;
-        this.projectDescription = projectDescription;
-        this.requirements = requirements;
+        this.projectDescription = projectDescription;     
+        this.requestDate=requestDate;
+        this.startDate=startDate;
+        this.budget=budget;
     }
     public int getRequestid() {
         return requestId;
+    }
+    public User getUser() {
+    	return user;
     }
 
     public Service getService() {
         return service;
     }
+    
     public String getProjectdescription() {
         return projectDescription;
     }
 
-    public String getRequiremnts() {
-        return requirements;
+   
+    public Date getRequestDate() {
+    	return requestDate;
+    	
+    }
+    public Date getStartDate() {
+    	return startDate;
+    }
+    public double getBudget() {
+    	return budget;
     }
     public void setProjectdescription(String projectDescription) {
         this.projectDescription = projectDescription;
     }
-    public void setRequirements(String requirements) {
-        this.requirements = requirements;
+   
+    public void setRequestDate(Date requestDate) {
+    	this.requestDate=requestDate;
+    	
+    }
+    public void setStartDate(Date startDate) {
+    	this.startDate=startDate;
+    }
+    public void setBudget(double budget) {
+    	this.budget=budget;
     }
 }
